33555a42c2097c62e7d6dadf52bdcfcb5b3ad4ee
Gen Edwards <gen@fakepanda.com>
2016-08-06 16:28:03 -0400

    create index.html

---
+++ README.md
@@ -0,0 +1,5 @@
+This is just a personal project to play around with React and webpack.
+
+Build with `npm run build` and open the index.html from a browser.
+
+To launch a server with hot-loader capabilities run `npm run dev` and go to `localhost:4001/webpack-dev-server/bundle`
--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -1,19 +1,22 @@
 module.exports = {
   context: __dirname + '/app',
-  entry: ['./app.js'],
-  output : {
-    filename: 'bundle.js',
-    path: __dirname + '/build',
+  entry: ['webpack/hot/only-dev-server', './app.js'],
+  devServer: {
+    contentBase: './public',
+    host: '0.0.0.0',
+    hot: true,
+    port: 4001,
+    publicPath: '/',
+    noInfo: true,
+
   },
 
-// TODO: chain jsx->build/[name].js
-//       -> dist/bundle.js
   module: {
     loaders: [
       {
         test : /\.jsx?$/,
         exclude : /node_modules/,
-        loaders : ['babel?{"presets":["es2015","react"]}']
+        loaders : ['react-hot', 'babel?{"presets":["es2015","react"]}']
       },
       {
         test : /\.css$/,
